d03bd684c77ac59f8306be163d1f4093a6911b7f
oscarali1985 <oscarali1985@hotmail.com>
2019-12-31 10:34:51 -0400

    Commit

--- Java/Test/Aleatorioint.js
+++
@@ -1,4 +0,0 @@
-function generateRandom(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  console.log(generateRandom(1,10))
--- Java/Test/Validar.js
+++
@@ -1,6 +0,0 @@
-function isOdd(myNumber)
-{
-	return !(myNumber % 2 == 0);
-}
-
-console.log(isOdd(45345));
